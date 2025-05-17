b46e65f27fa6515cc4b0cfec25854103b5ef108d
shayan <shaian.heidari@gmail.com>
2025-05-17 21:20:51 +0330

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./index.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="w-100 h-100 bg-red-500"> hello world</div>
+    </>
+  );
+}
+
+export default App;
