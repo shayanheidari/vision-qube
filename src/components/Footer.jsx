0b21b2493994fc13cbfe4b3cf27f53f0e2d7850b
shayan <shaian.heidari@gmail.com>
2025-05-18 14:50:30 +0330

    init

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="absolute bottom-0 left-0 w-screen h-[10vh] flex items-end">
+    <div className="absolute bottom-0 left-0 w-screen h-[10vh] hidden md:flex items-end">
       <div className="flex px-5 py-3 flex-row md:w-[40vw] w-full md:text-white md:bg-transparent ">
         <p className="font-extralight text-sm">&copy; VisionQubeLtd - 2025</p>
       </div>
